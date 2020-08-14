f15fb33eb38383c3f165cff9c729b63538d7e68c
Graham Fisher <tfisher4@nd.edu>
2020-08-13 23:50:11 -0400

    added support for splitting shifts in the shifts sheet

--- send_shifts.py
+++ send_shifts.py
@@ -13,6 +13,8 @@ from datetime import date, time, datetime
 import calendar
 from more_itertools import nth
 from sys import argv
+from copy import copy
+from functools import reduce
 
 ''' Helpful globals for easy access'''
 # If modifying these scopes, delete the file token.pickle.
@@ -38,7 +40,9 @@ Prints to stdout a GroupMe message requesting EAs for a group's next events.
 
 options:
     -g <group_num>              Generate message for group <group_num>'s next events
+    -n                          Generate message for a request after the first (pneumonic: not first)
     -m <custom_message>         Include <custom_message> in message greeting
+    -t                          Use testing spreadsheet instead of live SAO EA request spreadsheet
         '''.split('\n')[1:-1]))
     exit(ret_code)
 
@@ -105,8 +109,8 @@ def get_next_events(events, group, column_idxs=EVENT_COLUMN_IDXS):
                                                       events)))
     return next_group_events
 
-def message_from_events(events, schedule, custom_message, column_idxs=EVENT_COLUMN_IDXS):
-    preface = f'Hey guys! {custom_message}{" " if custom_message else ""}Here are our shifts for'
+def message_from_events(events, schedule, message, is_first_req, column_idxs=EVENT_COLUMN_IDXS):
+    preface = f'{message} {"Here are our shifts for" if is_first_req else "We still need to fill the following shifts for"}'
 
     events_list = list(events) # for some reason, calling list on my iterator caused it to be consumed so I needed to save the list
     message = f'{preface} {day_of_event(first(events_list))}:\n\n' + '\n\n'.join(map(lambda e: event_to_str(e, schedule), events_list))
@@ -120,16 +124,35 @@ def day_of_event(event, column_idxs=EVENT_COLUMN_IDXS):
 
 def event_to_str(event, schedule, column_idxs=EVENT_COLUMN_IDXS):
     rem_EAs = '-'.join(map(lambda ext: str(int(ext)-num_EAs_scheduled(event[column_idxs['event_name']], schedule)), event[column_idxs['num_EAs']].split('-'))) if event[column_idxs['num_EAs']] else '?'
-    return f'  {event[column_idxs["event_name"]]} - {event[column_idxs["location"]]}: {rem_EAs} EA{"" if rem_EAs != "?" and "-" not in rem_EAs and int(rem_EAs) == 1 else "s"} {clean_time_rg_str(event[column_idxs["report_time"]] or event[column_idxs["event_time"]])}'
 
-def event_to_datetime_range(event, column_idxs=EVENT_COLUMN_IDXS):
+    rem_shifts = reduce(lambda running, shift: subtract_shifts(running, {(shift if shift else event_to_datetime_rg(event)): 1}),
+                        scheduled_shifts(event[column_idxs['event_name']], schedule),
+                        {event_to_datetime_rg(event): event[column_idxs['num_EAs']] or '?'})
+
+    return f'  {event[column_idxs["event_name"]]} - {event[column_idxs["location"]]}: {rem_shifts_to_str(rem_shifts)}'
+
+def rem_shifts_to_str(rem_shifts):
+    delim = '\n    '
+    return (delim if len(rem_shifts) > 1 else '') + delim.join(map(lambda time_rg: ' - '.join(map(time_to_clean_str, time_rg)) + f': {(EAs := rem_shifts[time_rg])} EA{"" if EAs != "?" and "-" not in EAs and int(EAs) == 1 else "s"}', {k: v for k, v in rem_shifts.items() if v != '0'}))
+
+def time_to_clean_str(time):
+    return time.strftime('%I:%M%p')
+
+def event_to_datetime_rg(event, column_idxs=EVENT_COLUMN_IDXS):
     date = date_str_to_obj(event[column_idxs['date']])
-    time_range = map(lambda t_s: time_str_to_obj(t_s.rstrip().lstrip()), clean_time_rg_str(event[column_idxs['report_time']]).split('-'))
+    time_range = map(lambda t_s: time_str_to_obj(t_s.rstrip().lstrip()), clean_time_rg_str(event[column_idxs['report_time']] or event[column_idxs['event_time']]).split('-'))
 
     return tuple(map(lambda t_o: datetime.combine(date, t_o), time_range))
 
+# TODO: should this function take in a clean str or a regular str?
+#   - argument for clean: establish clear pipeline instead of composing functions in other functions
+def clean_time_rg_str_to_time_rg(clean_time_rg_str):
+    return map(lambda t_s: time_str_to_obj(t_s.rstrip().lstrip()), clean_time_rg_str.split('-')) if clean_time_rg_str else None
+
 def clean_time_rg_str(time_str):
-    print(time_str)
+    print(f'time_rg: {time_str}')
+    if not time_str:
+        return time_str
     time_regex  = re.compile(r'(?P<start_hr>\d{1,2})(?::(?P<start_min>\d{2}))?(?P<start_m>pm|PM|AM|am)?\s?-\s?(?P<end_hr>\d{1,2})(?::(?P<end_min>\d{2}))?(?P<end_m>pm|PM|AM|am)')
     time_pieces = time_regex.match(time_str).groupdict()
 
@@ -137,41 +160,78 @@ def clean_time_rg_str(time_str):
 
     return clean_time
 
-
-# ex: {8-11: 3}, {9-10: 2} |---> {8-9: 3, 9-10:2, 10-11:3}
-def subtract_EA_ranges(minuend, subtrahend):
+# test with ranges on different days (11pm-1am, eg)
+def subtract_shifts(minuend, subtrahend):
     ''' minuend, subtrahend are dicts with keys (start_datetime, end_datetime) and values <num_eas> '''
-    r1 = sorted(r1)
-    r2 = sorted(r2)
+    # protect against the unlikely occurence that the num EAs is not filled in but people have signed up for shifts
+    if minuend[first(minuend)] == '?':
+        return minuend
 
     difference = {}
 
-    for minu in sorted(minuend):
-        minu_rg = tuple(map(lambda s: time_str_to_obj(s.rstrip().lstrip()), minu.split('-')))
-        subt_rg = tuple(map(lambda s: time_str_to_obj(s.rstrip().lstrip()), subtrahend.split('-')))
+    subt_rg = first(subtrahend)
+    print(f'before reduce: {minuend}')
 
-        if not do_time_ranges_overlap(minu_rg, subt_rg):
-            continue
-        difference
-        if subt_rg[1] > minu_rg[1]:
-            # truncate subtrahend
-            subtrahend = {f'{minu_rg[0]} - {subt_rg[1]}'}
-            difference[0]
+    # requires 2 passes through minuend
+    # maybe create a single for loop 
+    # or reduce that returns a tuple
+    overlapping_rgs, difference = reduce(lambda running, time_rg: (running[0] + [time_rg], running[1]) if do_time_ranges_overlap(time_rg, subt_rg)
+                                                                  else (running[0], add_dicts(running[1], {time_rg: minuend[time_rg]})),
+                                        minuend,
+                                        ([], {}))
+    print(overlapping_rgs, difference, '\n')
+    #overlapping_rgs = filter(lambda time_rg: do_time_ranges_overlap(time_rg, subt_rg), minuend)
+    #difference      = {time_rg: minuend[time_rg] for time_rg in minuend if not do_time_ranges_overlap(time_rg, subt_rg)}
 
+    for minu_rg in sorted(overlapping_rgs):
+        #minu_rg = tuple(map(lambda s: time_str_to_obj(s.rstrip().lstrip()), minu.split('-')))
+        #subt_rg = tuple(map(lambda s: time_str_to_obj(s.rstrip().lstrip()), subtrahend.split('-')))
 
+        og_subt_rg_end = time_rg_end(subt_rg)
+        subt_rg = (time_rg_start(subt_rg), min(map(time_rg_end, (subt_rg, minu_rg))))
+        
+        difference[ (time_rg_start(minu_rg), time_rg_start(subt_rg)) ] = minuend[minu_rg]
+        # maybe break the subtracting in the next time line into own function
+        #difference[ (time_rg_start(subt_rg), time_rg_end(subt_rg))   ] = '-'.join(map(lambda ext: str(int(ext) - subtrahend[first(subtrahend)]), minuend[minu_rg].split('-')))
+        difference[ (time_rg_start(subt_rg), time_rg_end(subt_rg))   ] = subtract_EAs(minuend[minu_rg], subtrahend[first(subtrahend)])
+        difference[ (time_rg_end(subt_rg)  , time_rg_end(minu_rg))   ] = minuend[minu_rg]
 
-    while True:
-        curr1_st, curr1_end = tuple(map(lambda s: time_str_to_obj(s.rstrip().lstrip()), curr1.split('-')))
-        curr2_st, curr2_end = tuple(map(lambda s: time_str_to_obj(s.rstrip().lstrip()), curr2.split('-')))
-        if curr2_st > curr1_st and curr2_st < curr1_end:
-            pass
+        #pprint(difference)
+        #print()
 
+        # filter out time ranges of 0
+        # this has to go through everything alerady put in difference - in the future look into constructing a local difference that's added to the larger one at the end of loop iter
+        difference = {time_rg: difference[time_rg] for time_rg in difference if time_rg_end(time_rg) != time_rg_start(time_rg)}
 
-        # idea: for each subtrahend, filter the minuends s.t. we only grab those overlapping ones. then do the splitting/subtraction for those overlapping
+        #pprint(difference)
+
+        subt_rg = (time_rg_end(minu_rg), og_subt_rg_end)
+        if time_rg_start(subt_rg) >= time_rg_end(subt_rg):
+            return difference
+
+def subtract_EAs(needed_str, filling):
+    return '-'.join(map(lambda ext: str(int(ext) - filling), needed_str.split('-')))
 
 def do_time_ranges_overlap(tm_rg_1, tm_rg_2):
-    pass
-    
+    ''' it is assumed that tm_rg_1 is not after tm_rg_2 '''
+    if tm_rg_1[0] > tm_rg_2[0]:
+        return do_time_ranges_overlap(tm_rg_2, tm_rg_1)
+    return tm_rg_1[1] > tm_rg_2[0]
+
+def time_rg_start(tm_rg):
+    return tm_rg[0]
+
+def time_rg_end(tm_rg):
+    return tm_rg[1]
+
+def add_dicts(d1, d2):
+    ''' returns the result of updating one dict with another. has the added benefit of not modifying the  '''
+    to_return = copy(d1)
+    to_return.update(d2)
+    return to_return
+
+def compose(*functions):
+    return reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
 
 def time_str_to_obj(clean_time_str):
     ''' Returns time obj from a time string in the format \d{1,2}:\d{2}(am|pm) '''
@@ -188,7 +248,7 @@ def first(iterable):
     return nth(iterable, 0)
         
 def date_str_to_obj(date_str):
-
+    print(f'date_str: {date_str}')
     return datetime.strptime(date_str, "%A, %B %d, %Y").date()
 
     date_regex = re.compile(r'\w+, (?P<month>\w+) (?P<day>\d{1,2}), (?P<year>\d{4})')
@@ -197,9 +257,22 @@ def date_str_to_obj(date_str):
     date_pieces = date_regex.search(date_str).groupdict()
     return date(int(date_pieces['year']), list(calendar.month_name).index(date_pieces['month']), int(date_pieces['day']))
 
+def shift_str_to_time_rg_str(EA_shift_str):
+    print(f'shift: {EA_shift_str}')
+    try:
+        return re.search(r'\((.*)\)', EA_shift_str).group(1)
+    except AttributeError:
+        return ''
+
 def num_EAs_scheduled(event_name, schedule, column_idxs=SCHED_COLUMN_IDXS):
     event_sched = first(filter(lambda e: e[column_idxs['event_name']] == event_name, schedule))
     return len(event_sched[column_idxs['EAs']].split(',')) if len(event_sched) >= column_idxs['EAs'] + 1 else 0
+
+def scheduled_shifts(event_name, schedule, column_idxs=SCHED_COLUMN_IDXS):
+    sched_event = first(filter(lambda e: e[column_idxs['event_name']] == event_name, schedule))
+    return map(lambda t_rg: tuple(map(lambda t_o: datetime.combine(date_str_to_obj(sched_event[column_idxs['date']]), t_o), t_rg)) if t_rg else None,
+               map(compose(clean_time_rg_str_to_time_rg, clean_time_rg_str, shift_str_to_time_rg_str),
+                   sched_event[column_idxs['EAs']].split(','))) if len(sched_event) >= column_idxs['EAs'] + 1 else []
 
 def process_args():
     to_return = {}
@@ -209,6 +282,8 @@ def process_args():
             to_return['group'] = next(to_process)
         elif arg == '-m':
             to_return['message'] = next(to_process)
+        elif arg == '-n':
+            to_return['not_first'] = True
         elif arg == '-t':
             to_return['test'] = True
         elif arg in ['-h', '--help']:
@@ -224,17 +299,39 @@ def main():
     
     arg_dict = process_args()
     group = arg_dict.get('group', DEFAULT_GROUP)
-    custom_message = arg_dict.get('message', '')
+    custom_message = arg_dict.get('message', 'Hey guys!')
     use_test_sheet = arg_dict.get('test', False)
+    is_first_req   = not arg_dict.get('not_first', False)
 
     sheets_API_obj = get_sheets_API_obj()
     
     next_events = list(get_next_events(get_events(sheets_API_obj, use_test_sheet), group))
     
-    print('\n'.join(map(lambda e: f'{e}: {event_to_datetime_range(e)}', next_events)))
+    print('\n'.join(map(lambda e: f'{e}: {event_to_datetime_rg(e)}', next_events)))
+
+    #event_time  = (datetime.combine(date.min, time(8, 0)), datetime.combine(date.min, time(11, 0)))
+    #ea_shift    = (datetime.combine(date.min, time(9, 0)), datetime.combine(date.min, time(10, 0)))
+    #ea_shift_2  = (datetime.combine(date.min, time(8, 30)), datetime.combine(date.min, time(10, 30)))
+
+
+    #new_rg = subtract_shifts({event_time: '3'}, {ea_shift: 1})
+    #pprint(f'res: {new_rg}')
+    #newer_rg = subtract_shifts(new_rg, {ea_shift: 1})
+    #pprint(f'res: {newer_rg}')
+
+    #newerer_rg = subtract_shifts(newer_rg, {ea_shift_2: 1})
+    #pprint(f'res: {newerer_rg}')
+
+    #newererer_rg = subtract_shifts(newerer_rg, {event_time: 1})
+    #pprint(f'res: {newererer_rg}')
+
+    #multi_day_event = (datetime.combine(date(2020, 8, 3), time(21, 0)), datetime.combine(date(2020, 8, 4), time(2, 0)))
+    #md_ea_shift = (datetime.combine(date(2020, 8, 3), time(22, 30)), datetime.combine(date(2020, 8, 4), time(0, 0)))
+
+    #pprint(subtract_shifts({multi_day_event: '3'}, {md_ea_shift: 1}))
 
     if next_events:
-        print(message_from_events(next_events, get_schedule(sheets_API_obj, use_test_sheet), custom_message))
+        print(message_from_events(next_events, get_schedule(sheets_API_obj, use_test_sheet), custom_message, is_first_req))
     else:
         print("No upcoming events")
 
